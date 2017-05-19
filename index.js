e5b61734b9688189893724d4394b87b0beff93e3
Buddhika Geasman <buddhikageasman@ip-192-168-5-188.ec2.internal>
2017-05-19 16:53:17 -0500

    commander

--- index.js
+++ index.js
@@ -5,18 +5,28 @@ const fs = require('fs');
 const util = require('util');
 const exec = require('child_process').exec;
 const svgUtil = require('./util/utility');
+const path = require('path');
+
+/* commander */
+const program = require('commander');
+program
+    .version('0.0.1')
+    .option('-i, --input [dir]', 'Input Directory')
+    .option('-o, --output [dir]', 'Output Directory')
+    .option('-f, --file <file>', 'File Name')
+    .parse(process.argv);
 
 /* svg folder */
-const folder = './svgs/';
+const folder = path.resolve(`${program.input}`);
 
 /* file to be written */
-const writeFile = './icons.js';
+const writeFile = path.resolve(`${program.file}`);
 const wstream = fs.createWriteStream(writeFile);
 
 /* convert to path */
-const fireXML = (folder, file) => {
-  return `xmllint --xpath "string(//*[local-name()='path']/@d)" ${folder}${file} | tr -d ' '`;
-};
+const fireXML = (folder, file) => (
+  `xmllint --xpath "string(//*[local-name()='path']/@d)" ${folder}/${file} | tr -d ' '`
+);
 
 /* read dir and write to file */
 fs.readdir(folder, (err, files)=> {
@@ -26,7 +36,8 @@ fs.readdir(folder, (err, files)=> {
       if (err) return svgUtil.errMsg(stderr);
 
       svgUtil.msg(file);
-      wstream.write(`const ${svgUtil.variablify(file)} = '${stdout}'; \n\n`);
+      wstream.write(`export const ${svgUtil.variablify(file)} = '${stdout}';
+`);
     })
   );
 });
